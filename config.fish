391a2674674af24e5e218074a3b09aba0302bb00
timebigot <timebigot@gmail.com>
2016-04-16 15:43:58 -0400

    added fish aliases and openbox config

--- config.fish
+++ config.fish
@@ -1,6 +1,9 @@
 # alias
 
-alias l "ls -la"
-alias clr "clear"
-alias install "sudo pacman -S"
-alias uninstall "sudo pacman -R"
+alias l 'ls -la'
+alias g 'git'
+alias c 'clear'
+alias v 'vim'
+alias install 'sudo pacman -S'
+alias uninstall 'sudo pacman -R'
+alias vimrc = '~/Project/dotfiles/.vimrc'
